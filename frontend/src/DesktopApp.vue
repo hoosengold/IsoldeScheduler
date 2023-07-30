e8c16cb80239e4020b4a51b92fbc4ca195d35c6d
hoosengold <73896404+hoosengold@users.noreply.github.com>
2023-07-30 22:35:31 +0300

    Minor improvements

--- frontend/src/DesktopApp.vue
+++ frontend/src/DesktopApp.vue
@@ -34,83 +34,6 @@ a.container-fluid img {
   height: 100%;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-  text-decoration-line: underline;
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  border-left: 1px solid var(--color-border);
-  padding-left: 1rem;
-  padding-right: 1rem;
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-nav a:hover {
-  color: var(--vt-c-fdiba-blue-pale);
-}
-
-.dropdown-menu {
-  min-width: 10em;
-}
-
-.dropdown-item {
-  max-width: 0.8em;
-}
-
-.dropdown-item:hover {
-  text-decoration-line: underline;
-  color: var(--vt-c-black-mute);
-  margin-left: 10px;
-  max-width: 0.8em;
-}
-
-.dropdown-item:active {
-  background: transparent;
-}
-
-.dropdown-menu li {
-  position: relative;
-}
-
-.dropdown-menu .dropdown-submenu {
-  display: none;
-  position: absolute;
-  left: 100%;
-  top: -7px;
-}
-
-.dropdown-menu {
-  right: 100%;
-  left: auto;
-}
-
-.dropdown-menu > li:hover > .dropdown-submenu {
-  display: inline-block;
-}
-
-.dropdown-hover:hover > .dropdown-menu {
-  display: inline-block;
-}
-
-.dropdown-hover > .dropdown-toggle:active {
-  pointer-events: none;
-}
-
 @media (min-width: 1024px) {
   header {
     display: flex;
@@ -122,11 +45,6 @@ nav a:hover {
     display: inline-block;
     place-items: flex-start;
     flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    font-size: 1rem;
   }
 }
 
